711be477b128a22a958af68202541f49c4eb051d
Hi <Hi@192.168.0.14>
2020-02-05 23:36:15 -0500

    test

---
+++ New/src/pack/Class.java
@@ -0,0 +1,11 @@
+package pack;
+
+
+ public final class Class {
+	
+	public void m1()
+	{
+		System.out.println("Hello public");
+	}
+
+}
---
+++ New/src/pack1/Accesscheck.java
@@ -0,0 +1,13 @@
+package pack1;
+import pack.Class;
+
+public class Accesscheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class cla = new Class();
+		cla.m1();
+
+	}
+
+}
